5e72f89c9451a99051c10991576ce354f997e724
andyElking <andraz.jelincic@gmail.com>
2023-10-19 17:08:40 +0100

    merging VirtualSTLATree and VirtualBrownianTree

--- diffrax/brownian/base.py
+++ diffrax/brownian/base.py
@@ -1,14 +1,32 @@
 import abc
+from typing import Optional
+import equinox as eqx
+from dataclasses import field
 
 from ..custom_types import Array, PyTree, Scalar
 from ..path import AbstractPath
+
+
+class LevyVal(eqx.Module):
+    h: Scalar = field(default=0.0)
+    W: PyTree[Array] = field(default=None)
+    J: Optional[PyTree[Array]] = field(default=None)
+    H: Optional[PyTree[Array]] = field(default=None)
+
+    def wh(self):
+        return self.W, self.H
+
+    def wj(self):
+        return self.W, self.J
 
 
 class AbstractBrownianPath(AbstractPath):
     "Abstract base class for all Brownian paths."
 
     @abc.abstractmethod
-    def evaluate(self, t0: Scalar, t1: Scalar, left: bool = True) -> PyTree[Array]:
+    def evaluate(
+            self, t0: Scalar, t1: Optional[Scalar] = None, left: bool = True, use_levy: bool =False
+    ) -> PyTree[Array]:
         r"""Samples a Brownian increment $w(t_1) - w(t_0)$.
 
         Each increment has distribution $\mathcal{N}(0, t_1 - t_0)$.
