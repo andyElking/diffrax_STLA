e880943a7e1a6ee33cab308dea91cf6e04b68748
andyElking <andraz.jelincic@gmail.com>
2023-10-20 17:55:33 +0100

    trying to merge STLAMultiTerm into MultiTerm

--- diffrax/term.py
+++ diffrax/term.py
@@ -8,6 +8,7 @@ import jax.numpy as jnp
 import jax.tree_util as jtu
 import numpy as np
 from equinox.internal import ω
+from jax import lax
 
 from .custom_types import Array, PyTree, Scalar
 from .path import AbstractPath
@@ -67,6 +68,12 @@ class AbstractTerm(eqx.Module):
         represent $x(t_1) - x(t_0)$.
         """
         pass
+
+
+    def levy_contr(self, t0: Scalar, t1: Scalar) -> PyTree:
+        r""" Same as contr, except if it is a Brownian path it outputs LevyVal.
+        """
+        return self.contr(t0,t1)
 
     @abc.abstractmethod
     def prod(self, vf: PyTree, control: PyTree) -> PyTree:
@@ -213,6 +220,12 @@ class _ControlTerm(AbstractTerm):
     def contr(self, t0: Scalar, t1: Scalar) -> PyTree:
         return self.control.evaluate(t0, t1)
 
+    def levy_contr(self, t0: Scalar, t1: Scalar) -> LevyVal | PyTree:
+        if isinstance(self.control, AbstractBrownianPath):
+            return self.control.evaluate(t0, t1, use_levy=True)
+        else:
+            return self.control.evaluate(t0, t1)
+
     def to_ode(self) -> ODETerm:
         r"""If the control is differentiable then $f(t, y(t), args) \mathrm{d}x(t)$
         may be thought of as an ODE as
@@ -357,21 +370,30 @@ class MultiTerm(AbstractTerm, Generic[_Terms]):
     def contr(self, t0: Scalar, t1: Scalar) -> Tuple[PyTree, ...]:
         return tuple(term.contr(t0, t1) for term in self.terms)
 
+    def levy_contr(self, t0: Scalar, t1: Scalar) -> Tuple[PyTree, ...]:
+        return tuple(term.levy_contr(t0, t1) for term in self.terms)
+
     def prod(self, vf: Tuple[PyTree, ...], control: Tuple[PyTree, ...]) -> PyTree:
         out = [
-            term.prod(vf_, control_)
-            for term, vf_, control_ in zip(self.terms, vf, control)
+            # lax.cond(control_ == 0, lambda _: 0, lambda ctr: term.prod(vf, ctr), control_)
+            # (0 if control_ == 0 else term.prod(vf, control_))
+            # for term, vf_, control_ in zip(self.terms, vf, control)
+            term.prod(vf, control_)
+            for term, vf_, control_ in zip(self.terms, vf, control) if control_ != 0
         ]
-        return jtu.tree_map(_sum, *out)
+        return 0 if len(out) ==0 else jtu.tree_map(_sum, *out)
 
     def vf_prod(
             self, t: Scalar, y: PyTree, args: PyTree, control: Tuple[PyTree, ...]
     ) -> PyTree:
         out = [
+            # lax.cond(control_ == 0, lambda _: 0.0, lambda ctr: term.vf_prod(t, y, args, ctr), control_)
+            # (0 if control_ == 0 else term.vf_prod(t, y, args, control_))
             term.vf_prod(t, y, args, control_)
-            for term, control_ in zip(self.terms, control)
+            for term, control_ in zip(self.terms, control) if control_ != 0
+
         ]
-        return jtu.tree_map(_sum, *out)
+        return 0 if len(out) ==0 else jtu.tree_map(_sum, *out)
 
     def is_vf_expensive(
             self,
@@ -421,7 +443,6 @@ class STLAMultiTerm(MultiTerm, AbstractSTLATerm):
         self.non_stla_terms = non_stla_term
         self.stla_term = stla_term
 
-        # Ordering: first non-stla terms, then stla-terms
         self.terms = (self.non_stla_terms, self.stla_term)
 
     def stla_contr(self, t0: Scalar, t1: Scalar) -> (PyTree, PyTree):
