f4a5883b11644eeab4dc50237843f6990fddf0cd
andyElking <andraz.jelincic@gmail.com>
2023-11-01 10:56:19 +0000

    Reformatted and wrote some unit testing

--- test/test_align.py
+++ test/test_align.py
@@ -1,7 +1,6 @@
 import jax
 import jax.numpy as jnp
 import jax.random as jrandom
-import pytest
 from diffrax import (
     ALIGN,
     ControlTerm,
@@ -10,7 +9,6 @@ from diffrax import (
     MultiTerm,
     ODETerm,
     SaveAt,
-    ShARK,
     VirtualBrownianTree,
 )
 
@@ -28,7 +26,9 @@ def solutions(keys, sde, dt0, solver, stepsize_controller=None):
     _saveat = SaveAt(ts=[_t1])
 
     def end_value(key):
-        path = get_bm(sde, key)
+        path = VirtualBrownianTree(
+            t0=_t0, t1=_t1, shape=(w_dim,), tol=2**-9, key=key, compute_stla=True
+        )
         terms = get_terms(path)
         if stepsize_controller is None:
             sol = diffeqsolve(
@@ -60,9 +60,7 @@ def solver_distance(keys, sde, solver1, dt1, solver2, dt2):
 def solver_order(keys, sde, solver, correct_solver, dt_precise, hs_num=5, hs=None):
     correct_sols = solutions(keys, sde, dt0=dt_precise, solver=correct_solver)
     if hs is None:
-        hs = 0.025 * jnp.power(
-            jnp.float32(2.0), jnp.arange(0, hs_num, dtype=jnp.float32)
-        )
+        hs = 0.025 * jnp.power(jnp.float32(2.0), jnp.arange(0, hs_num))
 
     def get_single_err(h):
         sols = solutions(keys, sde, dt0=h, solver=solver)
@@ -92,19 +90,11 @@ def diffusion(t, y, args):
     return d_y
 
 
-def get_bm(sde, key):
-    _, _, _, y0, _t0, _t1, w_dim = sde
-    shp_dtype = jax.ShapeDtypeStruct((w_dim,), dtype=y0.dtype)
-    return VirtualBrownianTree(
-        t0=_t0, t1=_t1, shape=shp_dtype, tol=2**-9, key=key, compute_stla=True
-    )
-
-
 def get_terms(bm: VirtualBrownianTree):
     return MultiTerm(ODETerm(drift), ControlTerm(diffusion, bm))
 
 
-t0, t1 = 0.3, 5
+t0, t1 = 0.3, 15
 
 gamma_hosc = jnp.array([2, 0.5], dtype=jnp.float32)
 u_hosc = jnp.array([0.5, 2], dtype=jnp.float32)
@@ -115,62 +105,59 @@ harmonic_osc = (drift, diffusion, args_hosc, y0_hosc, t0, t1, w_dim_hosc)
 
 grad_f_bqp = lambda x: 4 * x * (jnp.square(x) - 1)
 args_bqp = (jnp.float32(0.8), jnp.float32(0.2), grad_f_bqp)
-y0_bqp = jnp.zeros((2,), dtype=jnp.float32)
+y0_bqp = jnp.array([0, 0], dtype=jnp.float32)
 w_dim_bqp = 1
 bqp = (drift, diffusion, args_bqp, y0_bqp, t0, t1, w_dim_bqp)
 
 
-@pytest.mark.parametrize("solver", [ALIGN(0.1), ShARK()])
-def test_shape(solver):
-    t0, t1 = 0.3, 1.0
-    for dtype in [jnp.float16, jnp.float32]:
-        saveat = SaveAt(ts=jnp.linspace(t0, t1, 10, dtype=dtype))
-        for dim in [1, 3]:
-            u = dtype(1.0)
-            gam = dtype(1.0)
-            vec_u = jnp.ones((dim,), dtype=dtype)
-            vec_gam = jnp.ones((dim,), dtype=dtype)
-            y0 = jnp.zeros((2 * dim,), dtype=dtype)
-            f = lambda x: 0.5 * x
-            shp_dtype = jax.ShapeDtypeStruct((dim,), dtype)
-            terms = get_terms(
-                VirtualBrownianTree(
-                    t0,
-                    t1,
-                    tol=2**-9,
-                    shape=shp_dtype,
-                    key=jrandom.PRNGKey(4),
-                    compute_stla=True,
-                )
+def test_shape():
+    dtype = jnp.float32
+    saveat = SaveAt(ts=jnp.linspace(t0, t1, 1000))
+    for dim in [1, 2, 5]:
+        u = dtype(1.0)
+        gam = dtype(1.0)
+        vec_u = jnp.ones((dim,), dtype=dtype)
+        vec_gam = jnp.ones((dim,), dtype=dtype)
+        y0 = jnp.zeros((2 * dim,), dtype=dtype)
+        f = lambda x: 0.5 * x
+        shp_dtype = jax.ShapeDtypeStruct((dim,), dtype)
+        terms = get_terms(
+            VirtualBrownianTree(
+                t0,
+                t1,
+                tol=2**-9,
+                shape=shp_dtype,
+                key=jrandom.PRNGKey(4),
+                compute_stla=True,
             )
-            for args in [
-                (gam, u, f),
-                (vec_gam, u, f),
-                (gam, vec_u, f),
-                (vec_gam, vec_u, f),
-            ]:
-                sol = diffeqsolve(
-                    terms, solver, t0, t1, dt0=0.3, y0=y0, args=args, saveat=saveat
-                )
-                assert sol.ys.shape == (10, 2 * dim)
-                assert sol.ys.dtype == dtype
+        )
+        for args in [
+            (gam, u, f),
+            (vec_gam, u, f),
+            (gam, vec_u, f),
+            (vec_gam, vec_u, f),
+        ]:
+            sol = diffeqsolve(
+                terms, ALIGN(0.1), t0, t1, dt0=0.3, y0=y0, args=args, saveat=saveat
+            )
+            assert sol.ys.shape == (1000, 2 * dim)
 
 
-@pytest.mark.parametrize("solver", [ALIGN(0.1), ShARK()])
-def test_convergence(solver):
+def test_convergence():
     num_samples = 1000
     keys = jrandom.split(jrandom.PRNGKey(2), num=num_samples)
-
     for sde in [harmonic_osc, bqp]:
-        hs = 0.1 * jnp.power(jnp.float32(2.0), jnp.arange(0, 4, dtype=jnp.float32))
-        _, errs, order_vs_euler = solver_order(
-            keys, sde, solver, Euler(), jnp.float32(0.005), hs=hs
+        print(sde)
+        hs = 0.1 * jnp.power(jnp.float32(2.0), jnp.arange(0, 4))
+        print("blip")
+        _, errs, order_align_v_euler = solver_order(
+            keys, sde, ALIGN(0.1), Euler(), 0.005, hs=hs
         )
-        assert errs[0] < (0.1 if isinstance(solver, ALIGN) else 0.3)
-        assert order_vs_euler > 1.3
+        assert errs[0] < 0.3
+        assert order_align_v_euler > 1.0
 
-        hs = 0.025 * jnp.power(jnp.float32(2.0), jnp.arange(0, 5, dtype=jnp.float32))
-        _, _, order_vs_itself = solver_order(
-            keys, sde, solver, solver, jnp.float32(0.005), hs=hs
+        hs = 0.025 * jnp.power(jnp.float32(2.0), jnp.arange(0, 5))
+        _, _, order_align_v_align = solver_order(
+            keys, sde, ALIGN(0.1), ALIGN(0.1), 0.005, hs=hs
         )
-        assert order_vs_itself > (1.9 if isinstance(solver, ALIGN) else 1.0)
+        assert order_align_v_align > 1.9
