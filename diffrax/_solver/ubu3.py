8fc0504efcd305bdcda516097292099de5548dc1
andyElking <andraz.jelincic@gmail.com>
2024-05-07 15:43:12 +0100

    ubu3 now works with pytrees

--- diffrax/_solver/ubu3.py
+++ diffrax/_solver/ubu3.py
@@ -1,10 +1,12 @@
 import math
+from typing import Callable
 
 import equinox as eqx
 import jax
 import jax.lax as lax
 import jax.numpy as jnp
 import jax.tree_util as jtu
+from equinox.internal import ω
 from jax import vmap
 from jaxtyping import Array, ArrayLike, PyTree
 
@@ -16,7 +18,7 @@ from .._custom_types import (
 )
 from .._local_interpolation import LocalLinearInterpolation
 from .._solution import RESULTS
-from .._term import LangevinTerm, LangevinTuple
+from .._term import LangevinTerm, LangevinTuple, LangevinX
 from .base import AbstractItoSolver
 
 
@@ -25,22 +27,34 @@ from .base import AbstractItoSolver
 
 
 class _Coeffs(eqx.Module):
-    beta_lr1: Array  # (gamma, 3, *taylor)
-    a_lr1: Array  # (gamma, 3, *taylor)
-    b_lr1: Array  # (gamma, 3, *taylor)
-    a_third: Array  # (gamma, 1, *taylor)
-    a_div_h: Array  # (gamma, 1, *taylor)
+    beta_lr1: PyTree[Array]  # (gamma, 3, *taylor)
+    a_lr1: PyTree[Array]  # (gamma, 3, *taylor)
+    b_lr1: PyTree[Array]  # (gamma, 3, *taylor)
+    a_third: PyTree[Array]  # (gamma, 1, *taylor)
+    a_div_h: PyTree[Array]  # (gamma, 1, *taylor)
 
     @property
     def dtype(self):
-        return self.beta_lr1.dtype
+        return jtu.tree_leaves(self.beta_lr1)[0].dtype
+
+
+_coeffs_structure = jtu.tree_structure(
+    _Coeffs(
+        beta_lr1=jnp.array(0.0),
+        a_lr1=jnp.array(0.0),
+        b_lr1=jnp.array(0.0),
+        a_third=jnp.array(0.0),
+        a_div_h=jnp.array(0.0),
+    )
+)
 
 
 class _SolverState(eqx.Module):
     h: RealScalarLike
-    taylor_coeffs: _Coeffs
+    taylor_coeffs: PyTree[_Coeffs, "LangevinX"]
     coeffs: _Coeffs
-    rho: Array
+    rho: PyTree[Array]
+    prev_f: LangevinX
 
 
 # CONCERNING COEFFICIENTS:
@@ -57,26 +71,26 @@ class _SolverState(eqx.Module):
 # one can compute the UBU coefficients more precisely for a small h.
 
 
-def _directly_compute_coeffs(h, gamma) -> _Coeffs:
+def _directly_compute_coeffs_leaf(h, c) -> _Coeffs:
     # compute the coefficients directly (as opposed to via Taylor expansion)
-    assert gamma.ndim in [0, 1]
-    original_shape = gamma.shape
-    gamma = jnp.expand_dims(gamma, axis=-1)
-    alpha = gamma * h
+    assert c.ndim in [0, 1]
+    original_shape = c.shape
+    c = jnp.expand_dims(c, axis=-1)
+    alpha = c * h
     l = 0.5 - math.sqrt(3) / 6
     r = 0.5 + math.sqrt(3) / 6
-    l_r_1 = jnp.array([l, r, 1.0], dtype=jnp.dtype(gamma))
-    if gamma.ndim == 1:
+    l_r_1 = jnp.array([l, r, 1.0], dtype=jnp.dtype(c))
+    if c.ndim == 1:
         jnp.expand_dims(l_r_1, axis=0)
     alpha_lr1 = alpha * l_r_1
     assert alpha_lr1.shape == original_shape + (
         3,
     ), f"expected {original_shape + (3,)}, got {alpha_lr1.shape}"
     beta_lr1 = jnp.exp(-alpha_lr1)
-    a_lr1 = (1.0 - beta_lr1) / gamma
-    b_lr1 = (beta_lr1 + alpha_lr1 - 1.0) / (gamma**2 * h)
-    a_third = (1.0 - jnp.exp(-alpha / 3)) / gamma
-    a_div_h = (1.0 - jnp.exp(-alpha)) / (gamma * h)
+    a_lr1 = (1.0 - beta_lr1) / c
+    b_lr1 = (beta_lr1 + alpha_lr1 - 1.0) / (c**2 * h)
+    a_third = (1.0 - jnp.exp(-alpha / 3)) / c
+    a_div_h = (1.0 - jnp.exp(-alpha)) / (c * h)
 
     assert a_third.shape == a_div_h.shape == original_shape + (1,)
 
@@ -89,7 +103,7 @@ def _directly_compute_coeffs(h, gamma) -> _Coeffs:
     )
 
 
-def _tay_cfs_single(c) -> _Coeffs:
+def _tay_cfs_single(c: Array) -> _Coeffs:
     # c is gamma
     assert c.ndim == 0
     dtype = jnp.dtype(c)
@@ -133,18 +147,18 @@ def _tay_cfs_single(c) -> _Coeffs:
     )
 
 
-def _comp_taylor_coeffs(gamma) -> _Coeffs:
+def _comp_taylor_coeffs_leaf(c) -> _Coeffs:
     # When the step-size h is small the coefficients (which depend on h) need
     # to be computed via Taylor expansion to ensure numerical stability.
     # This precomputes the Taylor coefficients (depending on gamma and u), which
     # are then multiplied by powers of h, to get the coefficients of ALIGN.
-    if jnp.ndim(gamma) == 0:
-        out = _tay_cfs_single(gamma)
+    if jnp.ndim(c) == 0:
+        out = _tay_cfs_single(c)
     else:
-        out = jax.vmap(_tay_cfs_single)(gamma)
+        out = jax.vmap(_tay_cfs_single)(c)
 
     def check_shape(x):
-        assert x.shape == gamma.shape + (3, 6) or x.shape == gamma.shape + (1, 6)
+        assert x.shape == c.shape + (3, 6) or x.shape == c.shape + (1, 6)
 
     jtu.tree_map(check_shape, out)
     return out
@@ -196,38 +210,48 @@ class UBU3(AbstractItoSolver):
     def strong_order(self, terms):
         return 3.0
 
-    def recompute_coeffs(self, h: RealScalarLike, gamma: Array, tay_cfs: _Coeffs):
-        # Used when the step-size h changes and coefficients need to be recomputed
-        # Depending on the size of h*gamma choose whether the Taylor expansion or
-        # direct computation is more accurate.
-        cond = h * gamma < self.taylor_threshold
-        if jnp.ndim(gamma) == 0:
-            return lax.cond(
-                cond,
-                lambda h_: _eval_taylor(h_, tay_cfs),
-                lambda h_: _directly_compute_coeffs(h_, gamma),
-                h,
-            )
-        else:
-            cond = jnp.expand_dims(cond, axis=-1)
-            tay_out = _eval_taylor(h, tay_cfs)
+    def recompute_coeffs(
+        self, h, gamma: LangevinX, tay_cfs: PyTree[_Coeffs]
+    ) -> _Coeffs:
+        def recompute_coeffs_leaf(c: ArrayLike, _tay_cfs: _Coeffs):
+            # Used when the step-size h changes and coefficients need to be recomputed
+            # Depending on the size of h*gamma choose whether the Taylor expansion or
+            # direct computation is more accurate.
+            cond = h * c < self.taylor_threshold
+            if jnp.ndim(c) == 0:
+                return lax.cond(
+                    cond,
+                    lambda h_: _eval_taylor(h_, _tay_cfs),
+                    lambda h_: _directly_compute_coeffs_leaf(h_, c),
+                    h,
+                )
+            else:
+                cond = jnp.expand_dims(cond, axis=-1)
+                tay_out = _eval_taylor(h, _tay_cfs)
 
-            def select_tay_or_direct(dummy):
-                fun = lambda gam: _directly_compute_coeffs(h, gam)
-                direct_out = vmap(fun)(gamma)
+                def select_tay_or_direct(dummy):
+                    fun = lambda _c: _directly_compute_coeffs_leaf(h, _c)
+                    direct_out = vmap(fun)(c)
 
-                def _choose(tay_leaf, direct_leaf):
-                    assert tay_leaf.ndim == direct_leaf.ndim == cond.ndim
-                    return jnp.where(cond, tay_leaf, direct_leaf)
+                    def _choose(tay_leaf, direct_leaf):
+                        assert tay_leaf.ndim == direct_leaf.ndim == cond.ndim
+                        return jnp.where(cond, tay_leaf, direct_leaf)
 
-                return jtu.tree_map(_choose, tay_out, direct_out)
+                    return jtu.tree_map(_choose, tay_out, direct_out)
 
-            # If all entries of h*gamma are below threshold, only compute tay_out
-            # otherwise, compute both tay_out and direct_out and select the
-            # correct one for each dimension
-            return lax.cond(
-                jnp.all(cond), lambda _: tay_out, select_tay_or_direct, None
-            )
+                # If all entries of h*gamma are below threshold, only compute tay_out
+                # otherwise, compute both tay_out and direct_out and select the
+                # correct one for each dimension
+                return lax.cond(
+                    jnp.all(cond), lambda _: tay_out, select_tay_or_direct, None
+                )
+
+        tree_with_cfs = jtu.tree_map(recompute_coeffs_leaf, gamma, tay_cfs)
+        outer = jtu.tree_structure(gamma)
+        inner = _coeffs_structure
+        cfs_with_tree = jtu.tree_transpose(outer, inner, tree_with_cfs)
+        assert isinstance(cfs_with_tree, _Coeffs)
+        return cfs_with_tree
 
     def init(
         self,
@@ -244,25 +268,107 @@ class UBU3(AbstractItoSolver):
         """
         assert isinstance(terms, LangevinTerm)
         gamma, u, f = terms.args  # f is in fact grad(f)
-        assert gamma.shape == u.shape
-        h = t1 - t0
-
-        tay_cfs = _comp_taylor_coeffs(gamma)
-        coeffs = self.recompute_coeffs(h, gamma, tay_cfs)
-        rho = jnp.sqrt(2 * gamma * u)
 
         x0, v0 = y0
-        assert x0.shape == v0.shape
-        assert x0.ndim in [0, 1]
+
+        def _check_shapes(_c, _u, _x, _v):
+            assert _x.ndim in [0, 1]
+            assert _c.shape == _u.shape == _x.shape == _v.shape
+
+        assert jtu.tree_all(jtu.tree_map(_check_shapes, gamma, u, x0, v0))
+
+        h = t1 - t0
+
+        tay_cfs = jtu.tree_map(_comp_taylor_coeffs_leaf, gamma)
+        # tay_cfs have the same tree structure as gamma, with each leaf being a _Coeffs
+        # and the arrays have an extra trailing dimension of 6
+
+        coeffs = self.recompute_coeffs(h, gamma, tay_cfs)
+        rho = jtu.tree_map(lambda c, _u: jnp.sqrt(2 * c * _u), gamma, u)
+
+        def check_coeff_shapes(_x, _bet_lr1, _a_third, _tay):
+            assert _bet_lr1.shape == _x.shape + (3,)
+            assert _a_third.shape == _x.shape + (1,)
+            assert isinstance(_tay, _Coeffs)
+
+        jtu.tree_map(check_coeff_shapes, x0, coeffs.beta_lr1, coeffs.a_third, tay_cfs)
 
         state_out = _SolverState(
             h=h,
             taylor_coeffs=tay_cfs,
             coeffs=coeffs,
             rho=rho,
+            prev_f=f(x0),
         )
 
         return state_out
+
+    def _compute_step(
+        self,
+        h: RealScalarLike,
+        levy: AbstractSpaceTimeTimeLevyArea,
+        x0: LangevinX,
+        v0: LangevinX,
+        f: Callable[[LangevinX], LangevinX],
+        u: LangevinX,
+        cfs: _Coeffs,
+        st: _SolverState,
+    ) -> tuple[LangevinX, LangevinX, LangevinX]:
+        w: LangevinX = levy.W
+        hh: LangevinX = levy.H
+        kk: LangevinX = levy.K
+
+        def _l(coeff):
+            return jtu.tree_map(lambda arr: arr[..., 0], coeff)
+
+        def _r(coeff):
+            return jtu.tree_map(lambda arr: arr[..., 1], coeff)
+
+        def _one(coeff):
+            return jtu.tree_map(lambda arr: arr[..., 2], coeff)
+
+        beta_l = _l(cfs.beta_lr1)
+        beta_r = _r(cfs.beta_lr1)
+        beta_1 = _one(cfs.beta_lr1)
+        a_l = _l(cfs.a_lr1)
+        a_r = _r(cfs.a_lr1)
+        a_1 = _one(cfs.a_lr1)
+        b_l = _l(cfs.b_lr1)
+        b_r = _r(cfs.b_lr1)
+        b_1 = _one(cfs.b_lr1)
+        a_third = _l(cfs.a_third)
+        a_div_h = _l(cfs.a_div_h)
+
+        rho_w_k = (st.rho**ω * (w**ω - 12 * kk**ω)).ω
+        uh = (u**ω * h).ω
+        v_tilde = (v0**ω + st.rho**ω * (hh**ω + 6 * kk**ω)).ω
+
+        x1 = (x0**ω + a_l**ω * v_tilde**ω + b_l**ω * rho_w_k**ω).ω
+        f1uh = (f(x1) ** ω * uh**ω).ω
+
+        x2 = (
+            x0**ω + a_r**ω * v_tilde**ω + b_r**ω * rho_w_k**ω - a_third**ω * f1uh**ω
+        ).ω
+        f2uh = (f(x2) ** ω * uh**ω).ω
+
+        x_out = (
+            x0**ω
+            + a_1**ω * v_tilde**ω
+            + b_1**ω * rho_w_k**ω
+            - 0.5 * (a_r**ω * f1uh**ω + a_l**ω * f2uh**ω)
+        ).ω
+
+        v_out_tilde = (
+            beta_1**ω * v_tilde**ω
+            - 0.5 * (beta_r**ω * f1uh**ω + beta_l**ω * f2uh**ω)
+            + a_div_h**ω * rho_w_k**ω
+        ).ω
+        v_out = (v_out_tilde**ω - st.rho**ω * (hh**ω - 6 * kk**ω)).ω
+
+        f_fsal = (
+            st.prev_f
+        )  # this method is not FSAL, but this is for compatibility with the base class
+        return x_out, v_out, f_fsal
 
     def step(
         self,
@@ -281,7 +387,7 @@ class UBU3(AbstractItoSolver):
         gamma, u, f = terms.args
 
         h_state = st.h
-        tay: _Coeffs = st.taylor_coeffs
+        tay: PyTree[_Coeffs] = st.taylor_coeffs
         cfs = st.coeffs
 
         # If h changed recompute coefficients
@@ -297,65 +403,23 @@ class UBU3(AbstractItoSolver):
         assert (
             levy.H is not None and levy.K is not None
         ), "The Brownian motion must have `levy_area=diffrax.SpaceTimeTimeLevyArea`"
-        w: ArrayLike = levy.W
-        hh: ArrayLike = levy.H
-        kk: ArrayLike = levy.K
 
         x0, v0 = y0
-        assert x0.shape == v0.shape
-        assert x0.ndim in [0, 1]
-        assert gamma.shape in [(), x0.shape]
-        assert cfs.beta_lr1.shape == gamma.shape + (3,)
-        assert cfs.a_third.shape == gamma.shape + (1,)
+        x_out, v_out, f_fsal = self._compute_step(h, levy, x0, v0, f, u, cfs, st)
 
-        def _l(coeff):
-            return coeff[..., 0]
+        def check_shapes_dtypes(_x, _v, _f, _x0):
+            assert _x.dtype == _v.dtype == _f.dtype == _x0.dtype, (
+                f"dtypes don't match. x0: {x0.dtype},"
+                f" v_out: {_v.dtype}, x_out: {_x.dtype}, f_fsal: {_f.dtype}"
+            )
+            assert _x.shape == _v.shape == _f.shape == _x0.shape, (
+                f"Shapes don't match. x0: {x0.shape},"
+                f" v_out: {_v.shape}, x_out: {_x.shape}, f_fsal: {_f.shape}"
+            )
 
-        def _r(coeff):
-            return coeff[..., 1]
-
-        def _one(coeff):
-            return coeff[..., 2]
-
-        beta = cfs.beta_lr1
-        a = cfs.a_lr1
-        b = cfs.b_lr1
-        a_third = cfs.a_third[..., 0]
-        a_div_h = cfs.a_div_h[..., 0]
-
-        rho_w_k = st.rho * (w - 12 * kk)
-        uh = u * h
-        v_tilde = v0 + st.rho * (hh + 6 * kk)
-
-        x1 = x0 + _l(a) * v_tilde + _l(b) * rho_w_k
-        f1uh = f(x1) * uh
-
-        x2 = x0 + _r(a) * v_tilde + _r(b) * rho_w_k - a_third * f1uh
-        f2uh = f(x2) * uh
-
-        x_out = (
-            x0
-            + _one(a) * v_tilde
-            + _one(b) * rho_w_k
-            - 0.5 * (_r(a) * f1uh + _l(a) * f2uh)
-        )
-
-        v_out_tilde = (
-            _one(beta) * v_tilde
-            - 0.5 * (_r(beta) * f1uh + _l(beta) * f2uh)
-            + a_div_h * rho_w_k
-        )
-        v_out = v_out_tilde - st.rho * (hh - 6 * kk)
+        jtu.tree_map(check_shapes_dtypes, x_out, v_out, f_fsal, x0)
 
         y1 = (x_out, v_out)
-        assert v_out.dtype == x_out.dtype == x0.dtype, (
-            f"dtypes don't match. x0: {x0.dtype},"
-            f" v_out: {v_out.dtype}, x_out: {x_out.dtype}"
-        )
-        assert x_out.shape == v_out.shape == x0.shape, (
-            f"Shapes don't match. x0: {x0.shape},"
-            f" v_out: {v_out.shape}, x_out: {x_out.shape}"
-        )
 
         # TODO: compute error estimate
 
@@ -365,6 +429,7 @@ class UBU3(AbstractItoSolver):
             taylor_coeffs=tay,
             coeffs=cfs,
             rho=st.rho,
+            prev_f=f_fsal,
         )
         return y1, None, dense_info, st, RESULTS.successful
 
